30972104d7fe9e08fe768e056a9a27f47c32e628
OMAR-AHMED-SAAD <110069095+OMAR-AHMED-SAAD@users.noreply.github.com>
2023-09-22 17:33:53 +0300

    added changes to deploy

--- api/server.js
+++ api/server.js
@@ -37,6 +37,7 @@ app.put('/image', image(db))
 
 app.post('/image', handleImageRecognition)
 
-app.listen(3000, () => {
-    console.log("running on port 3000");
+const PORT=process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`running on port ${PORT}`);
 })
